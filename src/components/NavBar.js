5b0846bcf06834b8e027c4e7cba291cde4dee27e
dranyl <dranyl@outlook.com>
2021-08-28 18:28:27 +1200

    changed color to green

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { SocialIcon } from 'react-social-icons';
 
 export default function NavBar() {
   return (
-    <header className='bg-red-600'>
+    <header className='bg-green-600'>
       <div className='container mx-auto flex justify-between'>
         <nav className='flex'>
           <NavLink
@@ -18,21 +18,21 @@ export default function NavBar() {
           <NavLink
             to='/post'
             className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800'
-            activeClassName='text-red-100 bg-red-700'
+            activeClassName='text-red-100 bg-green-700'
           >
             Blog Posts
           </NavLink>
           <NavLink
             to='/project'
             className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800'
-            activeClassName='text-red-100 bg-red-700'
+            activeClassName='text-red-100 bg-green-700'
           >
             Projects
           </NavLink>
           <NavLink
             to='/about'
             className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800'
-            activeClassName='text-red-100 bg-red-700'
+            activeClassName='text-red-100 bg-green-700'
           >
             About Me!
           </NavLink>
